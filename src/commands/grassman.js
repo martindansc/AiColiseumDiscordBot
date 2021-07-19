d6a5734b6bee6d005838860f76f0fe84eef4d915
Martín Dans <martindansc@gmail.com>
2021-07-20 00:38:54 +0200

    fixed grassman commands

--- src/commands/grassman.js
+++ src/commands/grassman.js
@@ -1,10 +1,10 @@
 let info = `
-The legend says the maps made by this infamous knight were the worst nightmare any bot could face. 
-From having your oponent in your face before you realise, to laberintic maps no one could ever traverse,
-full of agressive neutral warriorsa and canon towers that shot you from far distance everywere, 
-maps plenty of resources all along the path but just of one type.
-Says the legend that if you pay attention you can still find the infamous knight in this lands, you just need to pay attention
-and please, be cautios, you don't want to get trapped into one of his tricky... deadly maps.`;
+The legend says the maps made by this infamous knight were the worst nightmare any bot could face.
+From having your opponent in your face before you realise, to labyrinthic maps no one could ever traverse,
+full of aggressive neutral warriors and canon towers that shot you from all directions and
+maps plenty of resources all along the path but just of one type and none of the others.
+The legend says that if you stay around you can still find the infamous knight in these lands, 
+you just need to pay attention and please, be cautious, you don't want to get trapped into one of his tricky... deadly maps.`;
 
 module.exports = {
     name: 'grassman',
